b2c930ce81e6f4354efae3338aeb7806af6bec66
LorenzoLatinnePXL <11701160@student.pxl.be>
2024-10-03 08:06:07 +0200

    Added program layout. testNumeriekButton function added and working as intended. afsluitenButton function added and working as intended.

--- WPFCursusgeld/MainWindow.xaml.cs
+++ WPFCursusgeld/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace WPFCursusgeld
         {
             InitializeComponent();
         }
+
+        private void afsluitenButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void testNumeriekButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValidYear = int.TryParse(jaarTextBox.Text, out int year);
+            if (isValidYear)
+            {
+                isNumeriekTextBlock.Text = "Is numeriek.";
+                isNumeriekTextBlock.Visibility = Visibility.Visible;
+
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        isSchrikkeljaarTextBlock.Text = "Is een schrikkeljaar";
+                        isSchrikkeljaarTextBlock.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        isSchrikkeljaarTextBlock.Text = "Is geen schrikkeljaar";
+                        isSchrikkeljaarTextBlock.Visibility = Visibility.Visible;
+                    }
+                } else if (year % 4 == 0)
+                {
+                    isSchrikkeljaarTextBlock.Text = "Is een schrikkeljaar";
+                    isSchrikkeljaarTextBlock.Visibility = Visibility.Visible;
+                } else
+                {
+                    isSchrikkeljaarTextBlock.Text = "Is geen schrikkeljaar";
+                    isSchrikkeljaarTextBlock.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                isNumeriekTextBlock.Text = "Geef een correct jaartal!";
+                isNumeriekTextBlock.Visibility = Visibility.Visible;
+                isSchrikkeljaarTextBlock.Visibility = Visibility.Hidden;
+            }
+        }
     }
 }
